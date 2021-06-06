a321eb827248aa57a11f73369226cfd2a4db87cd
chuahan21 <80304726+chuahan21@users.noreply.github.com>
2021-06-06 12:01:26 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        The Bootcamp was for 7 days, we learned JAVASCRIPT and REACT.JS from
+        start. The whole Bootcamp was held by Shaurya sir. It was amazing to
+        learn everything from him. Waiting for more Bootcamps held by him in
+        future. THANKYOU SO MUCH SHAURYA SIR.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
